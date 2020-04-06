9f3e0325a73f5ecfa53515869909eed8ee128611
lpf-exclusive <lpf-exclusive@outlook.com>
2020-04-06 10:23:21 +0800

    环球快客采集

--- src/main/java/com/crawler/mapper/DataMapper.java
+++ src/main/java/com/crawler/mapper/DataMapper.java
@@ -1,11 +1,11 @@
 package com.crawler.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface DataMapper {
 
-    @Update("UPDATE server_ip SET real_ip =#{newIP},update_time =#{updateTime} WHERE host =#{oldHost};")
-    int saveIP(String newIP, String updateTime, String host);
+    @Insert("REPLACE INTO server_ip(`host`,real_ip,update_time) VALUES(#{host},#{newIP},NOW());")
+    int saveIP(String newIP, String host);
 }
