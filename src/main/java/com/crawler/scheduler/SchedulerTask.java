8cac2f412b39b21205734975d9f47d3313e3feec
lpf-exclusive <lpf-exclusive@outlook.com>
2020-04-07 11:48:19 +0800

    环球快客采集

--- src/main/java/com/crawler/scheduler/SchedulerTask.java
+++ src/main/java/com/crawler/scheduler/SchedulerTask.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -20,9 +19,8 @@ public class SchedulerTask {
     @Autowired
     private DataService dataService;
 
-    //cron表达式：每隔10秒执行一次
-    @Scheduled(cron = "0/10 * * * * *")
-    @Async("taskExecutor")
+    //cron表达式：每隔15秒执行一次
+    @Scheduled(cron = "0/15 * * * * *")
     public void scheduled() {
         boolean result = dataService.saveIP();
         if (result) {
