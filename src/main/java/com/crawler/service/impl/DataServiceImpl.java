0055b281fac17214b9266e11521fb6fbb073d20a
lpf-exclusive <lpf-exclusive@outlook.com>
2020-04-07 11:52:04 +0800

    环球快客采集

--- src/main/java/com/crawler/service/impl/DataServiceImpl.java
+++ src/main/java/com/crawler/service/impl/DataServiceImpl.java
@@ -3,6 +3,7 @@ package com.crawler.service.impl;
 import com.crawler.service.DataService;
 import com.crawler.utils.HttpUtils;
 import com.crawler.utils.IPUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,9 @@ public class DataServiceImpl implements DataService {
     @Override
     public boolean saveIP() {
         String realIP = IPUtils.getRealIP();//新IP
+        if (StringUtils.isBlank(realIP) || "null".equals(realIP)) {
+            return false;
+        }
         String url = "http://" + IP + ":" + PORT + "/saveIP.do?IDConfig=" + computerID + "&IP=" + realIP + "";
         String result = HttpUtils.doGet(url);
         if (result.contains("成功")) {
