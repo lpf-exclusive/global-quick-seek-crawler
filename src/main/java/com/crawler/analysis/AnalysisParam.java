a3ac1f3bf2df2fa882fd0b5e3a27abce33873568
lpf-exclusive <lpf-exclusive@outlook.com>
2020-04-14 17:53:34 +0800

    环球快客采集

--- src/main/java/com/crawler/analysis/AnalysisParam.java
+++ src/main/java/com/crawler/analysis/AnalysisParam.java
@@ -4,8 +4,12 @@ import com.crawler.utils.ReturnResult;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class AnalysisParam {
@@ -13,6 +17,18 @@ public class AnalysisParam {
     public static ReturnResult startAnalysisParam(HttpServletRequest request) {
         String param;
         ReturnResult returnResult = new ReturnResult();
+        /**
+         * 获取session
+         */
+        HttpSession httpSession = request.getSession();
+        Enumeration enumeration = httpSession.getAttributeNames();//获取session中所有的键值对
+        Map<String, String> map = new HashMap<>();
+        while (enumeration.hasMoreElements()) {
+            String sessionName = enumeration.nextElement().toString();//获取session中的键值
+            String sessionValue = httpSession.getAttribute(sessionName).toString();//根据键值取出session中的值
+            map.put(sessionName + sessionValue, "");
+        }
+
         /**
          * 处理关键词参数keyword
          */
@@ -52,26 +68,17 @@ public class AnalysisParam {
         /**
          * 处理页码参数page
          */
-        int page = new Random().nextInt(5) + 1;
-        param = param + "&first=" + ((page - 1) * 50);
-//        String page = request.getParameter("page");
-//        if (StringUtils.isBlank(page)) {
-//            returnResult.setCode("1");
-//            returnResult.setMsg("no page");
-//            return returnResult;
-//        } else {
-//            /**
-//             * 处理第几页类型参数pageNum
-//             */
-//            int pageNum = Integer.parseInt(page);
-//            if (pageNum == 1) {
-//                param = param + "&first=1";
-//            } else if (pageNum == 2) {
-//                param = param + "&first=11";
-//            } else if (pageNum > 2) {
-//                param = param + "&first=" + ((Integer.parseInt(page) - 1) * 14 - 3);
-//            }
-//        }
+        int page;
+        while (true) {
+            page = new Random().nextInt(15) + 1;
+            if (!map.containsKey(keyword + page)) {
+                System.out.println("没有采集过该页，开始采集。。。");
+                break;
+            }
+            System.out.println("已经采集过该页，重新选择页码。。。");
+        }
+        param = param + "&first=" + ((page - 1) * 10);
+        httpSession.setAttribute(keyword + page, "");
         returnResult.setCode("0");
         returnResult.setMsg("success");
         returnResult.setData(param);
