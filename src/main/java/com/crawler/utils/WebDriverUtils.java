2f87e039c35e3e59f7507f523c935bee15df9d03
lpf-exclusive <lpf-exclusive@outlook.com>
2020-04-14 15:22:01 +0800

    环球快客采集

--- src/main/java/com/crawler/utils/WebDriverUtils.java
+++ src/main/java/com/crawler/utils/WebDriverUtils.java
@@ -1,5 +1,6 @@
 package com.crawler.utils;
 
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -36,6 +37,9 @@ public class WebDriverUtils {
         chromeOptions.addArguments("blink-settings=imagesEnabled=false");
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get(website);
+        //设置cookies
+        Cookie c1 = new Cookie("SRCHHPGUSR", "CW=1920&CH=376&DPR=1&UTC=480&WTS=63722443621&HV=1586848021&NEWWND=1&NRSLT=50&SRCHLANG=&AS=1&NNT=1&HAP=0");
+        driver.manage().addCookie(c1);
         return driver;
     }
 }
